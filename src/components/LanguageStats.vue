b4d184539bcce72a5911fe53649b3b72abd43dc8
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-23 13:40:45 +0200

    Create LanguageStats.vue

---
+++ src/components/LanguageStats.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="language-stats">
+    <h4>Language Stats</h4>
+    <ul>
+      <li v-for="(percentage, language) in languageStats" :key="language">
+        {{ language }}: {{ percentage }}%
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    languageStats: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.language-stats {
+  margin-left: 20px;
+  font-size: 0.9em;
+}
+</style>
