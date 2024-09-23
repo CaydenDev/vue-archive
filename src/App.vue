fef73f9dcff17764bacf964dd898bbf18942bdc2
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-23 13:41:15 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>GitHub Repositories Viewer</h1>
+    <RepoList />
+  </div>
+</template>
+
+<script>
+import RepoList from './components/RepoList.vue';
+
+export default {
+  components: {
+    RepoList,
+  },
+};
+</script>
+
+<style>
+
+</style>
