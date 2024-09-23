1c89fdbd95420f212cc12a96669581d35b8af786
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-23 13:58:24 +0200

    Create Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome to My Vue App</h1>
+    <p>This is the main landing page.</p>
+    <router-link to="/repositories">View Repositories</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+
+</style>
