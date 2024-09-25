78be95cfc8f8b52a3f9c4e57e01a880e5a7cafaf
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-25 09:13:15 +0200

    Create color-picker.vue

---
+++ archive/color-picker.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Color Picker</h1>
+    <input type="color" v-model="selectedColor" />
+    <p style="color: selectedColor">Selected Color: {{ selectedColor }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedColor: '#ffffff'
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  margin-bottom: 10px;
+}
+</style>
