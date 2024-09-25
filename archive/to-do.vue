79bd2a7fe27365db8747628ed67bb3a9328ab2bc
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-25 09:12:02 +0200

    Create to-do.vue

---
+++ archive/to-do.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>My To-Do List</h1>
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task" />
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.text }}
+        <button @click="removeTask(task.id)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() === '') return;
+      this.tasks.push({ id: Date.now(), text: this.newTask });
+      this.newTask = '';
+    },
+    removeTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  margin-bottom: 10px;
+}
+button {
+  margin-left: 10px;
+}
+</style>
