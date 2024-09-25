d596191eef62f0b3c37a6f7e3373a29a556deec1
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-25 09:17:17 +0200

    Create pagination.vue

---
+++ archive/pagination.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="prevPage" :disabled="page === 1">Previous</button>
+    <span>Page {{ page }} of {{ totalPages }}</span>
+    <button @click="nextPage" :disabled="page === totalPages">Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: 1,
+      totalPages: 10 
+    };
+  },
+  methods: {
+    nextPage() {
+      if (this.page < this.totalPages) {
+        this.page++;
+      }
+    },
+    prevPage() {
+      if (this.page > 1) {
+        this.page--;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  margin: 0 5px;
+}
+</style>
