076bb40cbda35385976c0df132748a88aee8a59d
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-25 09:11:45 +0200

    Create counter.vue

---
+++ archive/counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Counter: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  margin: 5px;
+}
+</style>
