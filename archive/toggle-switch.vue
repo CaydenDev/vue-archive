e4133846168588467b4275be6fcf0b02b6f22daf
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-25 09:12:20 +0200

    Create toggle-switch.vue

---
+++ archive/toggle-switch.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label>
+      <input type="checkbox" v-model="isToggled" />
+      Toggle Switch
+    </label>
+    <p>The switch is: {{ isToggled ? 'ON' : 'OFF' }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isToggled: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+label {
+  font-size: 20px;
+}
+</style>
